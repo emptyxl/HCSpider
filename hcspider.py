c7443934585436660cc1bda2a59446967fc6f33d
empty_xl <empty_xl@163.com>
2018-06-17 01:56:37 +0800

    v0.1 single thread version

---
+++ hcspider.py
@@ -0,0 +1,311 @@
+# -*- coding: UTF-8 -*-
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from seleniumrequests import Chrome as post_Chrome
+from multiprocessing import Pool, Lock, Manager
+from urllib import parse
+from lxml import etree
+from parse_sim_url import remove_sim_url
+import re
+import random
+import time
+import logging
+import queue
+import pybloomfilter
+import multiprocessing
+import threadpool
+import threading
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, Text
+from sqlalchemy.orm import sessionmaker
+
+MAX_RECURSION_DEPTH = 1
+q = queue.Queue()
+url_bloom = pybloomfilter.BloomFilter(10000, 0.01, 'url.bloom')
+delay_time = [0, 2, 5, 10, 20, 60]
+
+# set logger
+logger = logging.getLogger('mylog')
+handler = logging.FileHandler('spider.log')
+console_handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    '[%(asctime)s] %(levelname)-8s in %(module)s: %(message)s')
+handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+handler.setLevel(logging.DEBUG)
+console_handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+logger.addHandler(console_handler)
+logger.setLevel(logging.DEBUG)
+
+# set database
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
+
+DB_CONNECT_STRING = 'mysql+mysqldb://root:@localhost/hcspider?charset=utf8'
+db = create_engine(DB_CONNECT_STRING, max_overflow=50)
+Base = declarative_base()
+Session = sessionmaker(bind=db)
+session = Session()
+
+class SpiderItem(Base):
+
+    __tablename__ = 'domain'
+
+    id = Column(Integer, primary_key=True)
+    method = Column(String(6), nullable=False)
+    url = Column(Text, nullable=False)
+    netloc = Column(String(255), nullable=False)
+    data = Column(Text, nullable=True, default=None)
+    deep = Column(Integer, nullable=False, default=0)
+
+    def __str__(self):
+        return 'Item [%s] %s' % (self.method, self.url[:20])
+
+
+def cookie2dict(cookie):
+    cookies = dict([l.split("=", 1) for l in cookie.split("; ")])
+    return cookies
+
+
+def calc_url_uuid(method, url):
+    u = parse.urlparse(url)
+    parmas = parse.parse_qs(u.query)
+    uuid = u.netloc + u.path
+    sorted_parmas = sorted([x for x in parmas])
+    uuid += '/' + '&'.join([x for x in sorted_parmas])
+    return uuid
+
+
+def rand_string(n):
+    s = 'abcdefghijglmnopqrstuvwxyz1234567890._-()*@'
+    res = random.sample(s[:26], 1)[0] + ''.join(random.sample(s, n))
+    return res
+
+
+def clean_up_url(orurl, current_url):
+    if orurl is None:
+        return None
+    else:
+        # https://a.b/c?d=e
+        if re.match('^http[s]?://[\s\S]+\.[\s\S]+', orurl):
+            return orurl
+        # //a.b/c?d=e
+        elif re.match('^//[^/][\s\S]*\.[\s\S]+', orurl):
+            sch = parse.urlparse(current_url).scheme
+            return sch + ":" + orurl
+        # /a.b/c?d=e
+        elif re.match('^/[^/][\s\S]*\.[\s\S]+', orurl):
+            return parse.urljoin(current_url, orurl[1:])
+        # javascript:;
+        elif re.match('^javascript:[\s\S]+', orurl, re.IGNORECASE):
+            return None
+        # a.b/c?d=e
+        elif re.match('^[^/]{2}[\s\S]+', orurl):
+            return parse.urljoin(current_url, orurl)
+        else:
+            return None
+
+
+def parse_form_input(type, name, value):
+    if name is None or name == '':
+        return None
+    if type == 'submit':
+        return None
+    if type == 'hidden':
+        if value is None:
+            return (name, rand_string(random.randint(10, 15)))
+        else:
+            return (name, value)
+
+    if value is not None and value != '':
+        return (name, value)
+    else:
+        # set different value
+        if type == 'number':
+            return (name, 7923476589)
+        elif type == 'date':
+            return (name, time.strftime("%Y-%m-%d"))
+        elif type == 'datetime':
+            return (name, time.strftime("%Y-%m-%d %H:%M:%S"))
+        else:
+            return (name, rand_string(random.randint(10, 15)))
+
+    return None
+
+
+def parse_page(tree, current_url, delay, deep, br):
+    tmp = []
+    global q, url_bloom
+    # get url in href
+    for link in tree.xpath("//@href"):
+        curl = clean_up_url(link, current_url)
+        if (curl is not None) and not re.match(r'[\s\S]*(\.png|\.jpg|\.css|\.gif|\.js|\.ico|\.xml|\.svg)$', parse.urlparse(link).path):
+            uuid = calc_url_uuid('get', link)
+            if uuid not in url_bloom:
+                # each item in queue consists of (method, url, data, delay, deep)
+                # store in tmp array to parse sim
+                tmp.append(('get', curl, None, delay, deep + 1))
+            url_bloom.add(uuid)
+
+    # get url in form
+    all_form = br.find_elements_by_xpath('//form')
+    for form in all_form:
+        form_url = clean_up_url(form.get_attribute('action'), br.current_url)
+        if form_url is None:
+            continue
+        method = form.get_attribute('method')
+        if method is None or method.lower() not in ['get', 'post']:
+            continue
+        else:
+            method = method.lower()
+        all_input = form.find_elements_by_xpath('.//input')
+        post_data = {}
+        for input_element in all_input:
+            type = input_element.get_attribute('type')
+            name = input_element.get_attribute('name')
+            value = input_element.get_attribute('value')
+            item_data = parse_form_input(type, name, value)
+            if item_data is not None:
+                post_data[item_data[0]] = item_data[1]
+
+        # Add to corresponding list
+        if method == 'get':
+            gurl = parse.urlunparse((parse.urlparse(form_url).scheme, parse.urlparse(form_url).netloc,
+                                     parse.urlparse(form_url).path, '', parse.urlencode(post_data), ''))
+            uuid = calc_url_uuid('get', gurl)
+            if uuid not in url_bloom:
+                tmp.append(('get', gurl, None, delay, deep + 1))
+            url_bloom.add(uuid)
+
+        elif method == 'post':
+            uuid = 'post/' + \
+                parse.urlparse(form_url).netloc + \
+                parse.urlparse(form_url).path
+            uuid += '/' + '&'.join(sorted([_ for _ in post_data]))
+            if uuid not in url_bloom:
+                q.put(('post', form_url, post_data, delay, deep + 1))
+                session.add(SpiderItem(method=method, url=form_url, netloc=parse.urlparse(form_url).netloc, data=post_data, deep=deep+1))
+                session.commit()
+                logger.info('add item: [%s] %s' %(method, form_url[:20]))
+            url_bloom.add(uuid)
+
+        path_list = [parse.urlparse(_[1]).path for _ in tmp]
+        sim_list = remove_sim_url(path_list)
+        for item in tmp:
+            if parse.urlparse(item[1]).path in sim_list:
+                q.put(item)
+                session.add(SpiderItem(method=item[0], url=item[1], netloc=parse.urlparse(item[1]).netloc, data=item[2], deep=item[4]))
+                session.commit()
+                logger.info('add item: [%s] %s' %(method, item[1][:20]))
+
+
+
+def get_url_hc(cookie):
+    global q, url_bloom
+    try:
+        RETRY = 0
+        while q.qsize() > 0:
+            item = q.get(timeout=5)
+            # each item in queue consists of (method, url, data, delay, deep)
+            method, url, data, delay, deep = item[0], item[1], item[2], item[3], item[4]
+            if deep >= MAX_RECURSION_DEPTH:
+                continue
+            time.sleep(delay)
+            # set config
+            chrome_options = Options()
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--disable-gpu")
+            chrome_options.add_argument('window-size=1200x600')
+
+            # get data
+            if method == 'get':
+                browser = webdriver.Chrome(
+                    chrome_options=chrome_options, executable_path="./chromedriver")
+                browser.set_page_load_timeout(15)
+                browser.set_script_timeout(15)
+                browser.implicitly_wait(10)
+                try:
+                    browser.get(url)
+                except:
+                    logger.error('get %s timeout' % url)
+                    continue
+
+                if cookie is not None:
+                    browser.delete_all_cookies()
+                    Cookies = cookie2dict(cookie)
+                    for key in Cookies:
+                        browser.add_cookie(
+                            {'name': key, 'value': Cookies[key]})
+                        try:
+                            browser.refresh()
+                        except:
+                            logger.error('get %s timeout' % url)
+                            continue
+
+                try:
+                    html = browser.page_source
+                    tree = etree.HTML(html)
+                except:
+                    continue
+
+                parse_page(tree, url, delay, deep, browser)
+
+            elif method == 'post':
+                browser = post_Chrome(
+                    chrome_options=chrome_options, executable_path="./chromedriver")
+                browser.set_page_load_timeout(15)
+                browser.set_script_timeout(15)
+                browser.implicitly_wait(10)
+
+                if cookie is not None:
+                    browser.delete_all_cookies()
+                    Cookies = cookie2dict(cookie)
+                    for key in Cookies:
+                        browser.add_cookie(
+                            {'name': key, 'value': Cookies[key]})
+                try:
+                    response = browser.request('POST', url, data=data)
+                    tree = etree.HTML(response.text)
+                except:
+                    logger.error('post %s timeout' % url)
+                    continue
+                parse_page(tree, url, delay, deep, browser)
+
+            browser.quit()
+    except Exception as e:
+        print(e)
+
+
+def start_spider(surl, deep=0, delay_level=0, method='get', data=None, cookie=None):
+    """
+    surl        strat url: "https://www.baidu.com/?a=1&b=2" / scheme is required
+    deep        recursion depth   [default = 0]
+    delay_level delay crawl level [default = 0] / scope:0-5
+    method      support get/post in lowercase [default = get]
+    data        post method parmas [default = None]
+    cookie      you can copy the chrome cookies field directly, we will parse it [default = None]
+
+    each item in queue consists of (method, url, data, delay, deep)
+    """
+    global q
+
+    # create database
+    Base.metadata.create_all(db)
+
+    start_url = surl
+    delay = delay_time[delay_level]
+    # create start item
+    q.put((method, start_url, data, delay, deep))
+    session.add(SpiderItem(method=method, url=start_url, netloc=parse.urlparse(start_url).netloc, data=data, deep=deep))
+    session.commit()
+    logger.info('add item: [%s] %s' %(method, start_url[:20]))
+    get_url_hc(cookie)
+
+
+if __name__ == '__main__':
+    start_spider('https://www.taobao.com')
---
+++ parse_sim_url.py
@@ -0,0 +1,66 @@
+#coding: utf-8
+import random
+import time
+import numpy as np
+import re
+import heapq
+from operator import itemgetter
+import os
+
+def num_or_str(s):
+    if s>='0' and s<='9':
+        return True
+    else:
+        return False
+
+def cut(s):
+    if len(s)==0:
+        return []
+    tmp =[]
+    tmp.append(s[0])
+    i = 1
+    while (i<len(s)):
+        if (num_or_str(s[i]) and num_or_str(tmp[-1])) or ((not num_or_str(s[i])) and (not num_or_str(tmp[-1]))):
+            tmp[-1] += s[i]
+        else:
+            tmp.append(s[i])
+        i += 1
+    return tmp
+
+
+def Cosine(list1, list2):
+    if len(list1) != len(list2):
+        return 0
+    dataA = np.mat(list1)
+    dataB = np.mat(list2)
+    sumData = dataA * dataB.T  # 若列为向量则为 dataA.T * dataB
+    denom = np.linalg.norm(dataA) * np.linalg.norm(dataB)
+    # 归一化
+    return 0.5 + 0.5 * (sumData / denom)
+
+
+def remove_sim_url(urllist):
+    tmp = []
+    for st in urllist:
+        tmpstr = os.path.basename(st)
+        res = cut(tmpstr)
+        feature = []
+        for item in res:
+            if re.match('[0-9]', item[0]):
+                feature.append(100 + len(item))
+            else:
+                feature.append(200 + len(item))
+
+        tmp.append((st, feature, len(feature)))
+    tmp = heapq.nlargest(len(tmp), tmp, itemgetter(2))
+    select_st = []
+    sp = []
+    if len(tmp) > 0:
+        sp.append(tmp[0])
+        for i in range(1, len(tmp)):
+            if Cosine(tmp[i][1], sp[-1][1]) < 0.7:
+                sp.append(tmp[i])
+        for _ in sp:
+            select_st.append(_[0])
+
+    return select_st
